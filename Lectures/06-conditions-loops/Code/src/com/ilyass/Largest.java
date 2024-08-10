a48689cd4a667f5696b105164bda9e59ab1bb9c1
ilyasseyounes <162178256+ilyasseyounes1@users.noreply.github.com>
2024-08-10 18:24:37 +0100

    Update Largest.java

--- Lectures/06-conditions-loops/Code/src/com/ilyass/Largest.java
+++ Lectures/06-conditions-loops/Code/src/com/ilyass/Largest.java
@@ -11,24 +11,25 @@ public class Largest {
         int c = in.nextInt();
 
         // Q: Find the largest of the 3 numbers
-//        int max = a;
-//        if (b > max) {
-//            max = b;
-//        }
-//        if (c > max) {
-//            max = c;
-//        }
+         int max = a;
+         if (b > max) {
+            max = b;
+          }
+         if (c > max) {
+            max = c;
+        }
 
-//        int max = 0;
-//        if (a > b) {
-//            max = a;
-//        } else {
-//            max = b;
-//        }
-//
-//        if (c > max) {
-//            max = c;
-//        }
+        int max = 0;
+        if (a > b) {
+            max = a;
+        } else {
+           max = b;
+        }
+
+        if (c > max) {
+             max = c;
+         }
+        // Method 2 : 
 
         int max = Math.max(c, Math.max(a, b));
 
