0431b9d98377b831cb136dac0898c5a14942efc0
ilyasseyounes <162178256+ilyasseyounes1@users.noreply.github.com>
2024-08-23 21:14:24 +0100

    recursion "RevNumber"

---
+++ Lectures/14-recursion/Code/src/ReverseNumber.java
@@ -0,0 +1,22 @@
+public class ReverseNumber {
+    // METHODE 1 : ****************************
+    static int sum = 0;
+    static void rev1(int n) {
+        if (n == 0) {
+            return;
+        }
+        int rem = n % 10;
+        sum = sum * 10 + rem;
+        rev1(n / 10);
+    }
+    //*********************************************** */
+
+
+    public static void main(String[] args) {
+        rev1(12345);
+        System.out.println(sum);
+
+    }
+
+
+}
