78f708f4a083ae49648ff1b04c12889700fd7f49
ilyasseyounes <162178256+ilyasseyounes1@users.noreply.github.com>
2024-08-31 21:36:02 +0100

    Delete Lectures/17-oop/Code/OPP2/OPP2 directory

--- Lectures/17-oop/Code/OPP2/OPP2/Singleton/Main.java
+++
@@ -1,8 +0,0 @@
-package OPP2.Singleton;
-
-public class Main {
-    public static void main(String[] args) {
-        Singleton obj =new Singleton();
-    }
-
-}
--- Lectures/17-oop/Code/OPP2/OPP2/Singleton/Singleton.java
+++
@@ -1,9 +0,0 @@
-package OPP2.Singleton;
-
-public class Singleton {
-    private Singleton () {
-
-    }
-    private Singleton instance; 
-
-}
