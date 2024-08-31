5e480c11c83808c1b604eb372dc625f42055043b
ilyasseyounes <162178256+ilyasseyounes1@users.noreply.github.com>
2024-08-31 21:32:09 +0100

    OPP2 packages code

---
+++ Lectures/17-oop/Code/OPP2/StaticExample/Packages/a/Greeting.java
@@ -0,0 +1,10 @@
+package OPP2.Packages.a;
+import static OPP2.Packages.b.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        message();
+    }
+
+}
