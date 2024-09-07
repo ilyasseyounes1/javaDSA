d746f4c59684ab3f02276b9f552fff49333d5f86
ilyasseyounes <162178256+ilyasseyounes1@users.noreply.github.com>
2024-09-07 17:40:26 +0100

    OPP3/ polymorphism and inheritance code

---
+++ Lectures/17-oop/Code/OPP3/Polymorphism/Shapes.java
@@ -0,0 +1,9 @@
+package OPP3.Polymorphism;
+
+public class Shapes{
+     void area (){
+         // final void area ()... : u cant override a final method u can also use it t prevalent   inheritance .
+         //Early binding : check notes ...
+        System.out.println ("I am in Shapes.");
+    }
+}
