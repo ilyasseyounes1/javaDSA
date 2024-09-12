bd3c86568c191c2c9341d77ba0baf1ea79ff66d8
ilyasseyounes <162178256+ilyasseyounes1@users.noreply.github.com>
2024-09-12 07:33:34 +0100

    extend Interface

---
+++ Lectures/17-oop/Code/OPP5/interfaces/extendDemo/B.java
@@ -0,0 +1,5 @@
+package OPP5.interfaces.extendDemo;
+
+public interface B extends A{
+    void gret();
+}
---
+++ Lectures/17-oop/Code/OPP5/interfaces/extendDemo/Main.java
@@ -0,0 +1,13 @@
+package OPP5.interfaces.extendDemo;
+
+public class Main implements B{
+    @Override
+    public void gret () {
+
+    }
+
+    @Override
+    public void fun () {
+
+    }
+}
