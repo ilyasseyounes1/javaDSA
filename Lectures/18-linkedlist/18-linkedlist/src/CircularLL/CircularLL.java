8844312ce2c5e58fb5f5a6512a671da823fe92de
ilyasseyounes <ilyasseyounes1@gmail.com>
2024-09-16 20:24:28 +0100

    CLL via control-v1

--- Lectures/18-linkedlist/18-linkedlist/src/CircularLL/CircularLL.java
+++ Lectures/18-linkedlist/18-linkedlist/src/CircularLL/CircularLL.java
@@ -1,56 +1,77 @@
 package CircularLL;
 
-public class CircularLL{
-    // in null here.
+public class CircularLL {
+
     private Node head;
-    private Node  tail;
-    private int size;
-    public void insertfirst (int val){
+    private Node tail;
+
+    public CircularLL() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insert(int val) {
         Node node = new Node(val);
-        node.next = head;
-        if(head != null){
+        if (head == null) {
             head = node;
-        }
-        size++;
-
-    }
-    public void insertlast (int val){
-        if(tail == null){
-            insertfirst(val);
-        }
-        Node node = new Node(val);
-        tail.next = node;
-        node.next = head;
-        size++;
-
-    }
-    public void Display (){
-        if (size == 0){
+            tail = node;
             return;
         }
-        Node temp = head;
-        while (temp != null){
-            System.out.println (temp.val+"->");
-            temp = temp.next;
-        }
-        System.out.println ("END");
+
+        tail.next = node;
+        node.next = head;
+        tail = node;
     }
 
+    public void display() {
+        Node node = head;
+        if (head != null) {
+            do {
+                System.out.print(node.val + " -> ");
+                if (node.next != null) {
+                    node = node.next;
+                }
+            } while (node != head);
+        }
+        System.out.println("HEAD");
+    }
 
+    public void delete(int val) {
+        Node node = head;
+        if (node == null) {
+            return;
+        }
 
+        if (head == tail){
+            head = null;
+            tail = null;
+            return;
+        }
 
+        if (node.val == val) {
+            head = head.next;
+            tail.next = head;
+            return;
+        }
+
+        do {
+            Node n = node.next;
+            if (n.val == val) {
+                node.next = n.next;
+                break;
+            }
+            node = node.next;
+        } while (node != head);
+
+    }
 
     private class Node {
         int val;
         Node next;
 
-        public Node ( int val ) {
+        public Node(int val) {
             this.val = val;
-        }
-
-        public Node ( int val , Node next ) {
-            this.val = val;
-            this.next = next;
         }
     }
 }
+
