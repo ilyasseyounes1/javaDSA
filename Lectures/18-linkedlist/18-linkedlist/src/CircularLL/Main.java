cbbdb76dbf9bcd9ae72f72c461c94ced12a13c75
ilyasseyounes <ilyasseyounes1@gmail.com>
2024-09-16 20:17:47 +0100

    circularLL add code

--- Lectures/18-linkedlist/18-linkedlist/src/CircularLL/Main.java
+++ Lectures/18-linkedlist/18-linkedlist/src/CircularLL/Main.java
@@ -5,8 +5,9 @@ public class Main{
         CircularLL cll = new CircularLL();
         cll.insertfirst ( 2 );
         cll.insertfirst ( 3 );
-        //cll.insertlast ( 5 );
-       // cll.insertlast ( 3 );
+        cll.insertlast ( 5 );
+        cll.insertlast ( 3 );
         cll.Display ();
+        cll.insertfirst ( 34 );
     }
 }
